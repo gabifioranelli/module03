69c07abab79356a313a740b55bf1237d4c6df354
gabifioranelli <gabifioranelli@gmail.com>
2020-10-05 15:00:13 -0300

    module03

---
+++ data.js
@@ -0,0 +1,39 @@
+module.exports = [
+    {
+        id: "2JctShrYmmo",
+        title: "Vídeo piloto",
+        duration: "34:11 min",
+        price: "Free",
+        featured: true
+    },
+    {
+        id: "G9Kqt7IQi0s",
+        title: "Mad Max: o Aiatolá do Rock'n'Roll",
+        duration: "26:35 min",
+        price: "Free"
+    },
+    {
+        id: "plGftWKkfws",
+        title: "SpaceBalls: Um louco por referências",
+        duration: "14:42 min",
+        price: "Free"
+    },
+    {
+        id: "fq0-ZEGEJqo",
+        title: "Jogos Vorazes: por uma sociedade melhor",
+        duration: "21:03 min",
+        price: "Free"
+    },
+    {
+        id: "xQT6aJMSx0g",
+        title: "Especial CCXP 2015: parte 1",
+        duration: "10:18 min",
+        price: "Free"
+    },
+    {
+        id: "QUVaULuFoLQ",
+        title: "Especial CCXP 2015: parte 2",
+        duration: "16:24 min",
+        price: "Free"
+    }
+]
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+const videos = require("./data")
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express:server,
+    autoescape: false,
+    noCache: true
+})
+
+server.get("/", function(req, res){
+    const about = {
+        avatar_url: "https://avatars2.githubusercontent.com/u/49372916?s=460&u=bd27c6460ee4bbf9119a847c8894626bc2f45914&v=4",
+        name: "Gabi Fioranelli",
+        role: "Estudante - Rocketseat",
+        description: 'Bióloga, estudante de programação pela <a href="Https://rocketseat.com.br" target="_blank">Rocketseat</a>',
+        links: [
+            { name: "GitHub", url: "https://github.com/gabifioranelli" },
+            { name: "Twitter", url: "http://www.twitter.com/gabigeek" },
+            { name: "LinkedIn", url: "https://www.linkedin.com/in/gabriellefioranelli/" }
+        ]
+    }
+    return res.render("about", { about })
+})
+
+server.get("/videos", function(req, res){
+    return res.render("videos", {items: videos})
+})
+
+server.get("/video", function(req, res) {
+    const id = req.query.id
+
+    const video = videos.find(function(video) {
+        if (video.id == id) {
+            return true
+        }
+    })
+
+    if (!video) {
+        return res.send("Video not found!")
+    }
+
+    return res.render("video", { item: video })
+})
+
+server.listen(5000, function(){
+    console.log("server is running")
+})
